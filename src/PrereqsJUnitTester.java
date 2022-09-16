6a79c601b2ecb714415300c5953252ca325b70a6
kuro <kenshimojo@icloud.com>
2022-09-15 18:51:07 -0700

    Writing tester, finished file creation and init test

--- src/PrereqsJUnitTester.java
+++ src/PrereqsJUnitTester.java
@@ -13,10 +13,20 @@ class PrereqsJUnitTester {
 	static void setUpBeforeClass() throws Exception {
 		File testText=new File("myTesterText.txt");
 		testText.createNewFile();
-		PrintWriter writer=new PrintWriter(testText);
-		writer.print("watch infinity train");
-		writer.close();
-		//assertTrue(testText.exists());
+		
+		File testText2=new File("myTesterText2.txt");
+		testText.createNewFile();
+		
+		File testText3=new File("myTesterText3.txt");
+		testText.createNewFile();
+		
+		PrintWriter writer2=new PrintWriter(testText2);
+		writer2.print("watch infinity train");
+		writer2.close();
+		
+		PrintWriter writer3=new PrintWriter(testText3);
+		writer3.print("watch mob psycho");
+		writer3.close();
 	}
 	//Charlie-s-GitPrereqs/objects
 
@@ -26,11 +36,33 @@ class PrereqsJUnitTester {
 		//testText.delete();
 		//kill blobs in objects, also index
 	}
-	
 	@Test
 	void testBlob() throws Exception {
+		File objectsFolder=new File("./objects");
+		objectsFolder.mkdir();
+		
 		Blob blobby=new Blob("myTesterText.txt");
-		File blobedText=new File("./objects/")
+		File blobbedText=new File("./objects/da39a3ee5e6b4b0d3255bfef95601890afd80709");
+		assertTrue(blobbedText.exists());
+		objectsFolder.delete();
+		
 	}
+	
+	
+	@Test //write one big test index tester ig
+	void testIndex() throws Exception {
+		Index dexy=new Index();
+		dexy.init();
+		
+		File objectsFolder=new File("./objects");
+		assertTrue(objectsFolder.exists());
+		File index=new File("index");
+		assertTrue(index.exists());
+		
+	}
+	//make interface to his code
+	
+	
+	
 
 }
